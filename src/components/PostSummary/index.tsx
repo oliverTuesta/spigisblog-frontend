ab9752910e1afe60b2f38eb09371ce6647ee978d
Oliver Tuesta <olvier.jtuesta@gmail.com>
2023-08-09 15:04:47 -0500

    feat: add Post summary component

---
+++ src/components/PostSummary/index.tsx
@@ -0,0 +1,37 @@
+import type { ReactElement } from 'react';
+import Image from 'next/image';
+
+export interface PostSummaryProps {}
+
+export default function PostSummary(props: PostSummaryProps): ReactElement {
+  return (
+    <>
+      <div className="divider"></div>
+      <div className="container flex gap-4 py-3">
+        <div className="">
+          <Image
+            className="rounded object-cover h-40 w-40"
+            alt="todo"
+            src="/images/post.png"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="flex flex-col justify-between">
+          <div>
+            <h3 className="text-2xl font-semibold">Post Title</h3>
+            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-sm text-gray-500">#test #test #test</p>
+            <div>
+              <p className="text-sm text-gray-500">
+                July 28, 2023 <span className="font-bold">SPIGI</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
