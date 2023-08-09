4de0a4d5bc4c4bdd9a1334c2db1c22a1f87136d2
Oliver Tuesta <olvier.jtuesta@gmail.com>
2023-08-09 15:05:53 -0500

    feat: add hashtags card

---
+++ src/components/HashtagsCard/index.tsx
@@ -0,0 +1,22 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/accordion';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+
+import type { ReactElement } from 'react';
+
+export interface CategoryCardProps {}
+
+export default function CategoriesCard(props: CategoryCardProps): ReactElement {
+  return (
+    <>
+      <Card className="bg-transparent">
+        <CardHeader>
+          <h3 className="font-bold text-2xl">Hashtags</h3>
+        </CardHeader>
+        <CardContent>
+          #test #test #lorem Elit voluptas quasi quidem molestias qui? Nemo voluptatem quas dolores obcaecati obcaecati.
+          Quasi repudiandae nesciunt?
+        </CardContent>
+      </Card>
+    </>
+  );
+}
