904fc417bac1d433419dc9b25ee7ad500be3852a
Oliver Tuesta <olvier.jtuesta@gmail.com>
2023-08-10 18:34:43 -0500

    fix: footer light color

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ export default function Footer(props: FooterProps): ReactElement {
   return (
     <>
       <div className="max-w-screen-lg container">
-        <footer className="flex flex-row justify-between text-gray-500 text-sm my-4">
+        <footer className="flex flex-row justify-between text-gray-400 text-sm my-4 dark:text-gray-500">
           <div>&copy; Spigi</div>
           <a href="https://x.com/SpigiPlant">SpigiPlant</a>
         </footer>
