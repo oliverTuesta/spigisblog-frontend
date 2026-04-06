46592c19f011ccbd9eefc00419b54b02ea71f5f5
spigi <oliver.jtuesta@gmail.com>
2026-04-05 20:21:42 -0500

    feat: update navbar games: add halo

---
+++ src/components/ReviewsNav.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+export default function ReviewsNav() {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger className="rounded-md">Reviews</NavigationMenuTrigger>
+
+                    <NavigationMenuContent>
+                        <ul className="grid gap-2 p-4" >
+                            <li>
+                                <NavigationMenuLink asChild>
+                                    <a href="/books" className="block p-2 hover:bg-muted rounded-md">
+                                        Books
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+                            <li>
+                                <NavigationMenuLink asChild>
+                                    <a href="/games" className="block p-2 hover:bg-muted rounded-md">
+                                        Games
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    );
+}
